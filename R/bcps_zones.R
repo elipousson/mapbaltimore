86815756e081b41a1776893615b399ea011aa77b
Eli Pousson <eli.pousson@gmail.com>
2020-12-05 00:51:58 -0500

    Add crop parameter to map_area_bcps_zones function

--- R/bcps_zones.R
+++ R/bcps_zones.R
@@ -3,6 +3,7 @@
 #' Map showing BCPS school zones that overlap with a provided area or areas. If the area sf tibble includes multiple areas, a separate map is created for each area provided.
 #'
 #' @param area Name of the neighborhood in sentence case.
+#' @param crop Default is "none." Only supported option is "standard" which uses the get_buffered_area function to crop the BCPS zones.
 #'
 #' @importFrom ggplot2 ggplot aes geom_sf
 #' @examples
@@ -20,7 +21,8 @@
 #' }
 #' @export
 
-map_area_bcps_zones <- function(area) {
+map_area_bcps_zones <- function(area,
+                                crop = "none") {
 
   check_area(area)
 
@@ -34,26 +36,148 @@ map_area_bcps_zones <- function(area) {
     ~ get_bcps_zones_for_area(.x)
   )
 
-  bcps_zone_maps <- purrr::map2(
-    area_nested$data,
-    area_nested$bcps_zone_data,
-    ~ map_bcps_zones_for_area(
-      area = .x,
-      area_bcps_zones = .y
-    )
-  )
+  if (crop == "none") {
 
-  return(bcps_zone_maps)
+    bcps_zone_maps <- purrr::map2(
+      area_nested$data,
+      area_nested$bcps_zone_data,
+      ~ # Create map of BCPS school zones intersecting the neighborhood
+        ggplot2::ggplot() +
+        # Map bcps_zones codes
+        ggplot2::geom_sf(
+          data = .y,
+          aes(fill = program_name),
+          color = NA
+        ) +
+        # Map neighborhood boundary
+        ggplot2::geom_sf(
+          data = .x,
+          color = "white",
+          fill = NA,
+          alpha = 0.8,
+          size = 1.6
+        ) +
+        # Map neighborhood boundary
+        ggplot2::geom_sf(
+          data = .x,
+          color = "gray20",
+          fill = NA,
+          linetype = 5
+        ) +
+        # TODO: Add school locations and/or building footprints
+        # Label school zones
+        ggrepel::geom_label_repel(
+          data = .y,
+          ggplot2::aes(
+            label = program_name,
+            fill = program_name,
+            geometry = geometry
+          ),
+          stat = "sf_coordinates",
+          colour = "white",
+          size = grid::unit(4, "lines"),
+          family = "Roboto Condensed",
+          point.padding = NA,
+          segment.color = "white",
+          label.size = grid::unit(0.5, "lines"),
+          label.padding = grid::unit(1, "lines"),
+          label.r = grid::unit(0, "lines")
+        ) +
+        # Define fill scale for school zones
+        ggplot2::scale_fill_viridis_d(end = 0.8) +
+        # Add title
+        ggplot2::labs(
+          title = glue::glue("{area$name}: Baltimore City Public School Attendance Zones")
+        ) +
+        ggplot2::guides(fill = "none") +
+        # Remove lat/lon axis text
+        ggplot2::theme_minimal() +
+        ggplot2::theme(
+          panel.grid.major = ggplot2::element_line(color = "transparent"),
+          axis.title = ggplot2::element_text(color = "transparent"),
+          axis.text = ggplot2::element_text(color = "transparent")
+        )
+    )
+  } else if (crop == "standard") {
+    bcps_zone_maps <- purrr::map2(
+      area_nested$data,
+      area_nested$bcps_zone_data,
+      ~ # Create map of BCPS school zones intersecting the neighborhood
+        ggplot2::ggplot() +
+        # Map bcps_zones codes
+        ggplot2::geom_sf(
+          data = sf::st_crop(.y, get_buffered_area(.x)),
+          aes(fill = program_name),
+          color = NA
+        ) +
+        # Map neighborhood boundary
+        ggplot2::geom_sf(
+          data = .x,
+          color = "white",
+          fill = NA,
+          alpha = 0.8,
+          size = 1.6
+        ) +
+        # Map neighborhood boundary
+        ggplot2::geom_sf(
+          data = .x,
+          color = "gray20",
+          fill = NA,
+          linetype = 5
+        ) +
+        # TODO: Add school locations and/or building footprints
+        # Label school zones
+        ggrepel::geom_label_repel(
+          data = sf::st_crop(.y, get_buffered_area(.x)),
+          ggplot2::aes(
+            label = program_name,
+            fill = program_name,
+            geometry = geometry
+          ),
+          stat = "sf_coordinates",
+          colour = "white",
+          size = grid::unit(4, "lines"),
+          family = "Roboto Condensed",
+          point.padding = NA,
+          segment.color = "white",
+          label.size = grid::unit(0.5, "lines"),
+          label.padding = grid::unit(1, "lines"),
+          label.r = grid::unit(0, "lines")
+        ) +
+        # Define fill scale for school zones
+        ggplot2::scale_fill_viridis_d(end = 0.8) +
+        # Add title
+        ggplot2::labs(
+          title = glue::glue("{area$name}: Baltimore City Public School Attendance Zones")
+        ) +
+        ggplot2::guides(fill = "none") +
+        # Remove lat/lon axis text
+        ggplot2::theme_minimal() +
+        ggplot2::theme(
+          panel.grid.major = ggplot2::element_line(color = "transparent"),
+          axis.title = ggplot2::element_text(color = "transparent"),
+          axis.text = ggplot2::element_text(color = "transparent")
+        )
+    )
+  }
+
+
+  if (length(bcps_zone_maps) == 1) {
+    return(bcps_zone_maps[[1]])
+  } else {
+    return(bcps_zone_maps)
+  }
+
 }
 
 get_bcps_zones_for_area <- function(area) {
 
   # Define a 1 meter buffer
-  buffer_1m <- units::set_units(1, m)
+  buffer <- units::set_units(-1, m)
 
   # Identify school zones that intersect area (excluding zones intersecting <= 1 meter)
   area_bcps_zones <- sf::st_join(bcps_zones,
-    sf::st_buffer(area, -1 * buffer_1m),
+    sf::st_buffer(area, buffer),
     join = sf::st_intersects
   )
 
@@ -61,57 +185,4 @@ get_bcps_zones_for_area <- function(area) {
   area_bcps_zones <- dplyr::filter(area_bcps_zones, !is.na(name))
 
   return(area_bcps_zones)
-}
-
-map_bcps_zones_for_area <- function(area,
-                                    area_bcps_zones) {
-
-  # Create map of BCPS school zones intersecting the neighborhood
-  ggplot2::ggplot() +
-    # Map bcps_zones codes
-    ggplot2::geom_sf(
-      data = area_bcps_zones,
-      aes(fill = program_name),
-      color = NA
-    ) +
-    # Map neighborhood boundary
-    ggplot2::geom_sf(
-      data = area,
-      color = "gray20",
-      fill = NA,
-      linetype = 5
-    ) +
-    # TODO: Add school locations and/or building footprints
-    # Label school zones
-    ggrepel::geom_label_repel(
-      data = area_bcps_zones,
-      ggplot2::aes(
-        label = program_name,
-        fill = program_name,
-        geometry = geometry
-      ),
-      stat = "sf_coordinates",
-      colour = "white",
-      size = grid::unit(4, "lines"),
-      family = "Roboto Condensed",
-      point.padding = NA,
-      segment.color = "white",
-      label.size = grid::unit(0.5, "lines"),
-      label.padding = grid::unit(1, "lines"),
-      label.r = grid::unit(0, "lines")
-    ) +
-    # Define color scale for school zones
-    ggplot2::scale_colour_viridis_d() +
-    # Add title
-    ggplot2::labs(
-      title = glue::glue("{area$name}: Baltimore City Public School Attendance Zones")
-    ) +
-    ggplot2::guides(fill = "none") +
-    # Remove lat/lon axis text
-    ggplot2::theme_minimal() +
-    ggplot2::theme(
-      panel.grid.major = ggplot2::element_line(color = "transparent"),
-      axis.title = ggplot2::element_text(color = "transparent"),
-      axis.text = ggplot2::element_text(color = "transparent")
-    )
 }

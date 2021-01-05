180c46e918747b4de72e7fb42833af7d828311b7
Eli Pousson <eli.pousson@gmail.com>
2021-01-05 00:41:48 -0500

    Expand map_area_bcps_zones function to make map_area_bcps_programs function

--- R/bcps_zones.R
+++ R/bcps_zones.R
@@ -1,6 +1,7 @@
-#' Map BCPS School Attendance zones for a local area
+#' Map BCPS programs and attendance zones for a local area
 #'
-#' Map showing BCPS school zones that overlap with a provided area or areas. If the area sf tibble includes multiple areas, a separate map is created for each area provided.
+#' Map showing BCPS school zones that overlap with a provided area or areas.
+#' If the area sf tibble includes multiple areas, a separate map is created for each area provided.
 #'
 #' @param area Name of the neighborhood in sentence case.
 #' @param crop Default is "none." Only supported option is "standard" which uses the get_buffered_area function to crop the BCPS zones.
@@ -11,53 +12,67 @@
 #' \dontrun{
 #' ## Map school attendance boundary zones for the Madison Park neighborhood
 #' madisonpark <- get_area(area_type = "neighborhood", area_name = "Madison Park")
-#' map_area_bcps_zones(area = madisonpark)
+#' map_area_bcps_programs(area = madisonpark)
 #' }
 #'
 #' \dontrun{
 #' ## Map school attendance boundary zones for City Council District 2
 #' district9 <- get_area(area_type = "council_district", area_name = "9")
-#' map_area_bcps_zones(area = district9)
+#' map_area_bcps_programs(area = district9)
 #' }
 #' @export
 
-map_area_bcps_zones <- function(area,
-                                crop = "none") {
+map_area_bcps_programs <- function(area) {
 
   check_area(area)
 
   area_nested <- dplyr::nest_by(area,
     name,
+    .key = "area",
     .keep = TRUE
   )
 
-  area_nested$bcps_zone_data <- purrr::map(
-    area_nested$data,
-    ~ get_bcps_zones_for_area(.x)
+  area_nested$bcps_programs <- purrr::map(
+    area_nested$area,
+    ~ get_bcps_programs_for_area(.x)
   )
 
   set_map_theme() # Set map theme
 
-  if (crop == "none") {
-
-    bcps_zone_maps <- purrr::map2(
-      area_nested$data,
-      area_nested$bcps_zone_data,
-      ~ # Create map of BCPS school zones intersecting the neighborhood
-        ggplot2::ggplot() +
-        # Map bcps_zones codes
+    bcps_program_maps <- purrr::map2(
+      area_nested$area,
+      area_nested$bcps_programs,
+      ~ ggplot2::ggplot() +
+        # Map BCPS attendance zones
         ggplot2::geom_sf(
-          data = .y,
+          data = .y$zones,
           aes(fill = program_name),
           color = NA
+        ) +
+        # Map BCPS school locations matched to zones
+        ggplot2::geom_sf(
+          data = .y$zoned_programs,
+          aes(fill = program_name),
+          shape = 21,
+          size = 4,
+          color = "white",
+          stroke = 1.5
+        ) +
+        # Map BCPS locations in area (not zoned)
+        ggplot2::geom_sf(
+          data = .y$other_programs,
+          shape = 21,
+          size = 4,
+          fill = "gray60",
+          color = "white"
         ) +
         # Map neighborhood boundary
         ggplot2::geom_sf(
           data = .x,
           color = "white",
           fill = NA,
-          alpha = 0.8,
-          size = 1.6
+          alpha = 0.6,
+          size = 1.8
         ) +
         # Map neighborhood boundary
         ggplot2::geom_sf(
@@ -66,111 +81,88 @@ map_area_bcps_zones <- function(area,
           fill = NA,
           linetype = 5
         ) +
-        # TODO: Add school locations and/or building footprints
-        # Label school zones
+        # Label zoned schools
         ggrepel::geom_label_repel(
-          data = .y,
+          data = .y$zoned_programs,
           ggplot2::aes(
             label = program_name,
             fill = program_name,
             geometry = geometry
           ),
           stat = "sf_coordinates",
-          colour = "white",
-          size = grid::unit(4, "lines"),
-          family = "Roboto Condensed",
-          point.padding = NA,
-          segment.color = "white",
-          label.size = grid::unit(0.5, "lines"),
-          label.padding = grid::unit(1, "lines"),
-          label.r = grid::unit(0, "lines")
-        ) +
-        # Define fill scale for school zones
-        ggplot2::scale_fill_viridis_d(end = 0.8) +
-        # Add title
-        ggplot2::labs(
-          title = glue::glue("{area$name}: Baltimore City Public School Attendance Zones")
-        ) +
-        ggplot2::guides(fill = "none")
-    )
-  } else if (crop == "standard") {
-    bcps_zone_maps <- purrr::map2(
-      area_nested$data,
-      area_nested$bcps_zone_data,
-      ~ # Create map of BCPS school zones intersecting the neighborhood
-        ggplot2::ggplot() +
-        # Map bcps_zones codes
-        ggplot2::geom_sf(
-          data = sf::st_crop(.y, get_buffered_area(.x)),
-          aes(fill = program_name),
-          color = NA
-        ) +
-        # Map neighborhood boundary
-        ggplot2::geom_sf(
-          data = .x,
           color = "white",
-          fill = NA,
-          alpha = 0.8,
-          size = 1.6
+          size = 4,
+          point.padding = 20,
+          min.segment.length = 0.25,
+          segment.color = "white",
+          label.size = 0.5,
+          label.padding = 0.5,
+          label.r = 0.05
         ) +
-        # Map neighborhood boundary
-        ggplot2::geom_sf(
-          data = .x,
-          color = "gray20",
-          fill = NA,
-          linetype = 5
-        ) +
-        # TODO: Add school locations and/or building footprints
-        # Label school zones
         ggrepel::geom_label_repel(
-          data = sf::st_crop(.y, get_buffered_area(.x)),
+          data = .y$other_programs,
           ggplot2::aes(
             label = program_name,
-            fill = program_name,
             geometry = geometry
           ),
           stat = "sf_coordinates",
-          colour = "white",
-          size = grid::unit(4, "lines"),
-          family = "Roboto Condensed",
-          point.padding = NA,
+          fill = "white",
+          color = "gray30",
+          size = 4,
+          point.padding = 20,
+          min.segment.length = 0.25,
           segment.color = "white",
-          label.size = grid::unit(0.5, "lines"),
-          label.padding = grid::unit(1, "lines"),
-          label.r = grid::unit(0, "lines")
+          label.size = 0.5,
+          label.padding = 0.5,
+          label.r = 0.05
         ) +
         # Define fill scale for school zones
-        ggplot2::scale_fill_viridis_d(end = 0.8) +
+        # ggplot2::scale_fill_viridis_d(end = 0.8) +
         # Add title
         ggplot2::labs(
           title = glue::glue("{area$name}: Baltimore City Public School Attendance Zones")
         ) +
-        ggplot2::guides(fill = "none")
+        ggplot2::guides(fill = "none",
+                        color = "none")
     )
-  }
 
 
-  if (length(bcps_zone_maps) == 1) {
-    return(bcps_zone_maps[[1]])
+  if (length(bcps_program_maps) == 1) {
+    return(bcps_program_maps[[1]])
   } else {
-    return(bcps_zone_maps)
+    return(bcps_program_maps)
   }
 
 }
 
-get_bcps_zones_for_area <- function(area) {
+get_bcps_programs_for_area <- function(area) {
 
-  # Define a 1 meter buffer
-  buffer <- units::set_units(-1, m)
+  check_area(area)
 
   # Identify school zones that intersect area (excluding zones intersecting <= 1 meter)
-  area_bcps_zones <- sf::st_join(bcps_zones,
-    sf::st_buffer(area, buffer),
-    join = sf::st_intersects
+  area_bcps_zones <- bcps_zones %>%
+    sf::st_join(
+      sf::st_buffer(area, units::set_units(-1, m)), # Define a 1 meter buffer
+      join = sf::st_intersects
+      ) %>%
+    dplyr::filter(!is.na(name)) %>%  # Filter to school zones that intersect neighborhood
+    dplyr::select(program_name:zone_name)
+
+  # Identify schools matching intersecting zones
+  area_bcps_zoned_programs <- bcps_programs %>%
+    dplyr::filter(program_number %in% area_bcps_zones$program_number)
+
+  # Add other schools within the area
+  area_bcps_other_programs <- bcps_programs %>%
+    sf::st_intersection(area) %>%
+    dplyr::filter(!(program_number %in% area_bcps_zoned_programs$program_number)) %>%
+    dplyr::select(program_name:zone_name)
+
+  area_bcps_programs <- list(
+    "zones" = area_bcps_zones,
+    "zoned_programs" = area_bcps_zoned_programs,
+    "other_programs" = area_bcps_other_programs
   )
 
-  # Filter to school zones that intersect neighborhood
-  area_bcps_zones <- dplyr::filter(area_bcps_zones, !is.na(name))
-
-  return(area_bcps_zones)
+  return(area_bcps_programs)
 }

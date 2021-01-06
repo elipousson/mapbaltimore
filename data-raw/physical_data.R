a13de6466a275c3dee1798335990748d2f3aa442
Eli Pousson <eli.pousson@gmail.com>
2021-01-05 21:39:39 -0500

    Update column names for parks

--- data-raw/physical_data.R
+++ data-raw/physical_data.R
@@ -4,20 +4,14 @@
 parks_path <- "https://services1.arcgis.com/UWYHeuuJISiGmgXx/ArcGIS/rest/services/Baltimore_City_Recreation_and_Parks/FeatureServer/2"
 
 # Import parks data with esri2sf package
-parks <- esri2sf::esri2sf(parks_path)
-
-# Clean column names
-parks <- janitor::clean_names(parks, "snake")
-
-# Transform to projected CRS
-parks <- sf::st_transform(parks, 2804)
-
-# Select relevant columns
-parks <- dplyr::select(parks,
-                        park_id, name, address, name_alt, bcrp, geoms)
-
-# Make valid to avoid "Ring Self-intersection" error when cropped
-parks <- sf::st_make_valid(parks)
+parks <- esri2sf::esri2sf(parks_path) %>%
+  sf::st_transform(2804) %>%  # Transform to projected CRS
+  sf::st_make_valid() %>% # Make valid to avoid "Ring Self-intersection" error
+  janitor::clean_names("snake") %>%  # Clean column names
+  dplyr::select(name, id = park_id, address, name_alt, operator = bcrp, geometry = geoms) %>% # Select relevant columns
+  dplyr::mutate(
+    operator = dplyr::if_else(operator == "Y", "Baltimore City Department of Recreation and Parks", "Other")
+  )
 
 usethis::use_data(parks, overwrite = TRUE)
 

25b3de915cfc28ed0b984cc2f29af4f0bc3002a7
Eli Pousson <eli.pousson@gmail.com>
2021-01-23 22:11:19 -0500

    Update real property
    - Removes derived subcat fields based on state data
    - Adds additional/duplicative state data fields
    - Contains quality issues that still need to be documented

--- data-raw/real_property.R
+++ data-raw/real_property.R
@@ -1,51 +1,99 @@
 ## code to prepare `real_property` dataset
 
+# NOTE: There are data quality issues with the city real property data that are not fully resolved.
+# 3887 of records from the city real property data have a duplicate account id.
+# 0 records in the state real property data have a duplicate account id.
+# The state metadata is joined to the city polygon data via the account id.
+
 real_property_pts_local_path <- "~/Downloads/PLAN_ParcelPoints_MDP/BACI/BACI.shp"
 real_property_pts <- sf::read_sf(real_property_pts_local_path)
 
 real_property_pts <- real_property_pts %>%
   sf::st_transform(2804)
 
-real_property_pts_key <- real_property_pts %>%
-  dplyr::select(acctid, resityp, descciuse, desclu, desccnst, descstyl, descbldg) %>%
+real_property_pts <- real_property_pts %>%
   dplyr::mutate(
-    descciuse_cat = stringr::str_extract(descciuse, "^[:upper:]+(?=[:space:])"),
-    descciuse_subcat = stringr::str_extract(descciuse,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
-    desccnst_cat = stringr::str_extract(desccnst, "^[:upper:]+(?=[:space:])"),
-    desccnst_subcat = stringr::str_extract(desccnst,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
-    descstyl_cat = stringr::str_extract(descstyl, "^[:upper:]+(?=[:space:])"),
-    descstyl_subcat = stringr::str_extract(descstyl,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
-    descbldg_cat = stringr::str_extract(descbldg, "^[:upper:]+(?=[:space:])"),
-    descbldg_subcat = stringr::str_extract(descbldg,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
-    descbldg_subcat = dplyr::if_else(descbldg_subcat == "RESTAURANT",
-                                     stringr::str_to_title(descbldg_subcat),
-                                     descbldg_subcat)
+    tradate = lubridate::ymd(tradate)
   ) %>%
-  dplyr::select(acctid, ct2010, bg2010, address,
-                resityp,
-                descciuse, descciuse_cat, descciuse_subcat,
-                desclu,
-                desccnst, desccnst_cat, desccnst_subcat,
-                descstyl, descstyl_cat, descstyl_subcat,
-                descbldg, descbldg_cat, descbldg_subcat)
+  select(c(acctid,ct2010,bg2010,ooi,resityp,address,strtnum,strtdir,strtnam,strttyp,strtsfx,strtunt,addrtyp,city,zipcode,ownname1,ownname2,namekey,ownadd1,ownadd2,owncity,ownstate,ownerzip,ownzip2,premsnum,premsdir,premsnam,premstyp,premcity,premzip,premzip2,section,block,lot,map,grid,parcel,zoning,znchgdat,rzrealdat,ciuse,descciuse,exclass,descexcl,lu,desclu,acres,landarea,luom,width,depth,pfuw,pfus,pflw,pfsp,pfsu,pfic,pfih,recind,yearblt,sqftstrc,strugrad,descgrad,strucnst,desccnst,strustyl,descstyl,strubldg,descbldg,lastinsp,lastassd,assessor,transno1,tradate,considr1,mortgag1,nfmlndvl,nfmimpvl,bldg_story,bldg_units,resi2010,resi2000,resi1990,resiuths,aprtment,trailer,special,other,ptype,sdatwebadr,existing,mdpvdate,legal3,homqlcod,resident,nfmttlvl,sdatdate)) %>%
+  select(-c(block, lot, section, assessor))
+
+# real_property_pts_key <- real_property_pts %>%
+#  dplyr::mutate(
+#    descciuse_cat = stringr::str_extract(descciuse, "^[:upper:]+(?=[:space:])"),
+#    descciuse_subcat = stringr::str_extract(descciuse,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
+#    desccnst_cat = stringr::str_extract(desccnst, "^[:upper:]+(?=[:space:])"),
+#    desccnst_subcat = stringr::str_extract(desccnst,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
+#    descstyl_cat = stringr::str_extract(descstyl, "^[:upper:]+(?=[:space:])"),
+#    descstyl_subcat = stringr::str_extract(descstyl,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
+#    descbldg_cat = stringr::str_extract(descbldg, "^[:upper:]+(?=[:space:])"),
+#    descbldg_subcat = stringr::str_extract(descbldg,"(?<=^[:upper:]{2,20}[:space:]{1}).+"),
+#    descbldg_subcat = dplyr::if_else(descbldg_subcat == "RESTAURANT",
+#                                     stringr::str_to_title(descbldg_subcat),
+#                                     descbldg_subcat)
+#  ) %>%
+#  dplyr::select(acctid, ct2010, bg2010, address,
+#                resityp,
+#                descciuse, descciuse_cat, descciuse_subcat,
+#                desclu,
+#                desccnst, desccnst_cat, desccnst_subcat,
+#                descstyl, descstyl_cat, descstyl_subcat,
+#                descbldg, descbldg_cat, descbldg_subcat)
 
 # usethis::use_data(real_property_pts, overwrite = TRUE)
 
 # Set path to City of Baltimore Open GIS Data Real Property
-# https://gis-baltimore.opendata.arcgis.com/datasets/b41551f53345445fa05b554cd77b3732_0
+
 # This data is a subset of the statewide data available from the Maryland State Department of Assessment and Taxation.
 # NOTE: This data is updated on a rolling basis with sales up through two weeks prior to access.
 # The last downloaded date should be included in the data.R file so the currency of the data is clear to users.
-# real_property_path <- "https://opendata.arcgis.com/datasets/b41551f53345445fa05b554cd77b3732_0.geojson"
+# https://data.baltimorecity.gov/datasets/real-property-information
+# real_property_path <- "https://geodata.baltimorecity.gov/egis/rest/services/CityView/Realproperty/MapServer/0"
+# real_property <- esri2sf::esri2sf(real_property_path)
 
-download.file(real_property_path, "real_property.geojson")
+# real_property_path <- "https://opendata.arcgis.com/datasets/3b7e32867c5b471683fd4294bfe29e37_0.geojson"
+# download.file(real_property_path, "real_property.geojson")
 
 # Import real_property data from downloaded 'real_property.geojson' file
-real_property <- sf::read_sf("real_property.geojson") %>%
+real_property <- sf::read_sf("real_property.geojson")
+
+real_property <- real_property %>%
   # Clean column names
   janitor::clean_names("snake") %>%
   # Transform to projected CRS
   sf::st_transform(2804)
+
+real_property <- real_property %>%
+  dplyr::mutate(
+    dplyr::across(where(is.character), ~ stringr::str_trim(.x)), # Trim owner, ward, section, block, and lot columns
+    ward = stringr::str_pad(ward, width = 2, side = "left", pad = "0"),
+    section = stringr::str_sub(section, start = 1, end = 2),
+    block = dplyr::if_else(
+      stringr::str_detect(block, "[:upper:]$"),
+      stringr::str_pad(block, width = 5, side = "left", pad = "0"),
+      stringr::str_pad(block, width = 4, side = "left", pad = "0")
+    ),
+    lot = dplyr::if_else(
+      stringr::str_detect(lot, "[:upper:]"),
+      stringr::str_pad(lot, width = 4, side = "left", pad = "0"),
+      stringr::str_pad(lot, width = 3, side = "left", pad = "0")
+    ),
+    acctid = dplyr::if_else(
+      stringr::str_detect(block, "[:upper:]$"),
+      paste0("03", ward, section, block, lot),
+      paste0("03", ward, section, block, " ", lot)
+    ),
+    acctid = stringr::str_trim(acctid)
+  ) %>%
+  tidyr::replace_na(list(no_imprv = "N", vacind = "N")) %>%
+  naniar::replace_with_na(replace = list(saledate = "00000000"))
+  dplyr::mutate(
+    # Set structure area to 0 when a property has no improvements
+    structarea = dplyr::case_when(no_imprv == "Y" && structarea != 0 ~ 0,
+                                 TRUE ~ structarea),
+    # Parse sale date
+    saledate = lubridate::mdy(saledate)
+  )
 
 # NOTE: All data from boundary_data must be loaded before this script is run.
 real_property_matched <- real_property %>%
@@ -83,35 +131,11 @@ real_property_matched <- real_property %>%
   ) %>%
   sf::st_drop_geometry()
 
-real_property <- real_property %>%
-  dplyr::left_join(real_property_matched, by = "objectid")
+real_property_merge <- real_property %>%
+  dplyr::left_join(real_property_matched, by = "objectid") %>%
+  dplyr::left_join(sf::st_drop_geometry(real_property_pts), by = "acctid")# %>%
+#  dplyr::select(-check)
 
-real_property <- real_property %>%
-  dplyr::mutate(
-    ward = stringr::str_trim(ward),
-    section = stringr::str_trim(section),
-    block = stringr::str_trim(block),
-    lot = stringr::str_trim(lot),
-    ward = stringr::str_pad(ward, width = 2, side = "left", pad = "0"),
-    section = stringr::str_sub(section, start = 1, end = 2),
-    block = dplyr::if_else(
-      stringr::str_detect(block, "[:upper:]$"),
-      stringr::str_pad(block, width = 5, side = "left", pad = "0"),
-      stringr::str_pad(block, width = 4, side = "left", pad = "0")
-    ),
-    lot = dplyr::if_else(
-      stringr::str_detect(lot, "[:upper:]"),
-      stringr::str_pad(lot, width = 4, side = "left", pad = "0"),
-      stringr::str_pad(lot, width = 3, side = "left", pad = "0")
-    ),
-    acctid = dplyr::if_else(
-      stringr::str_detect(block, "[:upper:]$"),
-      paste0("03", ward, section, block, lot),
-      paste0("03", ward, section, block, " ", lot)
-    ),
-    acctid = stringr::str_trim(acctid)
-  ) %>%
-  dplyr::left_join(sf::st_drop_geometry(real_property_pts_key), by = "acctid") %>%
-  dplyr::select(-check)
+real_property <- real_property_merge
 
 usethis::use_data(real_property, overwrite = TRUE)

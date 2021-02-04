c385c18c631a8b0b40d6c6623ae47e5556031fee
Eli Pousson <eli.pousson@gmail.com>
2021-02-04 13:41:37 -0500

    Update get_area_crashes to optionally download person and vehicle details

--- R/maryland_open_data.R
+++ R/maryland_open_data.R
@@ -169,10 +169,14 @@ get_maryland_open_resource <- function(resource = NULL,
 #' @export
 get_area_crashes <- function(area,
                              start_year = 2020,
-                             end_year = NULL,
+                             end_year = 2020,
                              geometry = FALSE,
-                             trim = FALSE) {
+                             trim = FALSE,
+                             type = c("crash", "person", "vehicle")) {
 
+  type <- match.arg(type)
+
+  resource <- "65du-s3qu"
 
   area_bbox <- sf::st_bbox(sf::st_transform(area, 4326))
 
@@ -182,7 +186,7 @@ get_area_crashes <- function(area,
   crashes <- purrr::map_dfr(
     c(start_year:end_year),
     ~ get_maryland_open_resource(
-      resource = "65du-s3qu",
+      resource = resource,
       where = glue::glue(
         "(year = '{.x}') AND (county_desc like 'Baltimore City') AND {where_bbox}"
         ),
@@ -193,6 +197,35 @@ get_area_crashes <- function(area,
     crashes <- sf::st_intersection(crashes, sf::st_union(area))
   }
 
-  return(crashes)
+  if (type == "person") {
+    resource <- "py4c-dicf"
+  } else if (type == "vehicle") {
+    resource <- "mhft-5t5y"
+  }
+
+  if (type != "crash") {
+    area_report_no <- paste0("'",paste0(crashes$report_no, collapse = "','"),"'")
+
+    type_data <- purrr::map_dfr(
+      c(start_year:end_year),
+      ~ get_maryland_open_resource(
+        resource = resource,
+        where = glue::glue("(year = '{.x}') AND report_no in({area_report_no})")
+        )
+    )
+
+    type_data <- type_data %>%
+      dplyr::left_join(crashes, by = c("report_no", "year", "quarter"))
+
+    if (geometry) {
+      type_data <- type_data %>%
+        sf::st_as_sf(sf_column_name = "geometry")
+    }
+
+    message("If multiple vehicles or persons are involved in a crash, the data on the crash represented by the unique report number will appear in multiple rows. Use type = 'crash' for a list of crash reports only.")
+    return(type_data)
+  } else {
+    return(crashes)
+  }
 
 }

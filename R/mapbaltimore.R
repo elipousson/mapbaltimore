c18fd1cde444e4e5ab2c6a3061bf0dd4b2c8f16b
Eli Pousson <eli.pousson@gmail.com>
2021-02-15 17:16:38 -0500

    Add package name script

---
+++ R/mapbaltimore.R
@@ -0,0 +1,11 @@
+#' mapbaltimore: A package for making maps with open data in Baltimore, Maryland.
+#'
+#' The mapbaltimore package is designed to provide an easy way to create maps of
+#' Baltimore neighborhoods, Council districts, and other areas using open data on
+#' schools, housing, parks, public transit, and more.
+#'
+#' @docType package
+#' @name mapbaltimore
+#' @import sf
+#' @importsFrom ggplot2 ggplot geom_sf aes
+NULL

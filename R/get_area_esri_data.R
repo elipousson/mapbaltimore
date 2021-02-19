5fdb4cc08063bd79d7b07adef58bca7fb86d8c53
Eli Pousson <eli.pousson@gmail.com>
2021-02-19 00:47:16 -0500

    Update get_area_esri_data to support dist, asp, and diag_ratio parameters

--- R/get_area_esri_data.R
+++ R/get_area_esri_data.R
@@ -1,14 +1,14 @@
 #' Get data from an ArcGIS FeatureServer or MapServer
 #'
-#' Wraps the `esri2sf::esri2sf()` function to download an ArcGIS FeatureServer or MapServer.
-#' Some of the data (e.g. Liquor Licenses) is missing data important data.
+#' Wraps the \code{esri2sf::esri2sf()} function to download an ArcGIS FeatureServer or MapServer.
 #'
-#' @param area `sf` object. Optional. Only used if trim is TRUE.
-#' @param bbox `bbox` object. Optional but suggested to avoid downloading entire layer. See `sf::st_bbox()` for more information.
+#' @param area \code{sf} object. Optional. Only used if trim is TRUE.
+#' @param bbox \code{bbox} object. Optional but suggested to avoid downloading entire layer. See `sf::st_bbox()` for more information.
 #' @param url FeatureServer or MapServer url to retrieve data from. Passed to `url` parameter of `esri2sf::esri2sf()` function.
 #' @param type Type of data to get. Options include "md food stores 2017 2018", "farmers markets 2020", "baltimore food stores 2016", "baltimore demolitions", "contour 2ft", "contours 10ft", "open vacant building notices", "liquor licenses", "fixed speed cameras", "red light cameras", and "edge of pavement"
 #' @param trim Logical. Default `FALSE.` If `TRUE`, area is required.
 #' @param crs Coordinate reference system. Default 2804.
+#' @inheritParams adjust_bbox
 #' @export
 #' @importFrom esri2sf esri2sf
 #' @importFrom dplyr filter pull rename
@@ -18,6 +18,9 @@ get_area_esri_data <- function(area = NULL,
                                bbox = NULL,
                                url = NULL,
                                type = c("md food stores 2017 2018", "farmers markets 2020", "baltimore food stores 2016", "baltimore demolitions", "contour 2ft", "contours 10ft", "open vacant building notices", "liquor licenses", "fixed speed cameras", "red light cameras", "edge of pavement"),
+                               dist = NULL,
+                               diag_ratio = NULL,
+                               asp = NULL,
                                trim = FALSE,
                                crs = 2804) {
 
@@ -26,40 +29,44 @@ get_area_esri_data <- function(area = NULL,
 
   if (is.null(url)) {
     # Get URL for FeatureServer or MapServer from internal esri_sources data
-    esri_url <- esri_sources %>%
+    url <- data_index %>%
       dplyr::filter(slug == type) %>%
       dplyr::pull(url)
-  } else {
-    esri_url <- url
   }
 
-  # Get bbox if area is provided
-  if (!is.null(area)) {
+  # Get adjusted bounding box if any adjustment variables provided
+  if (!is.null(dist) | !is.null(diag_ratio) | !is.null(asp)) {
+    bbox <- adjust_bbox(
+      area = area,
+      bbox = bbox,
+      dist = dist,
+      diag_ratio = diag_ratio,
+      asp = asp
+    )
+  } else if (!is.null(area)) {
     bbox <- sf::st_bbox(area)
   }
 
   # Get spatial data as sf using bbox if provided
   if (is.null(bbox)) {
-    esri_data <- esri2sf::esri2sf(url = esri_url)
-  } else if (class(bbox) == "bbox") {
-    esri_data <- esri2sf::esri2sf(url = esri_url, bbox = bbox)
+    data <- esri2sf::esri2sf(url = url)
   } else {
-    stop("The value for bbox is not a class 'bbox' object. Use sf::st_bbox() to create the bbox.")
+    data <- esri2sf::esri2sf(url = url, bbox = bbox)
   }
 
-  # Rename geometry field
-  esri_data <- esri_data %>%
+  data <- data %>%
     janitor::clean_names("snake") %>%
-    dplyr::rename(geometry = geoms) %>%
     sf::st_transform(crs)
+
+  if (!("geometry" %in% names(data))) {
+    message("The data does not include a column named 'geometry'.")
+  }
 
   # Optionally trim to area
   if (trim & !is.null(area)) {
-    esri_data <- esri_data %>%
+    data <- data %>%
       sf::st_intersection(area)
-  } else if (trim) {
-    warning("trim is TRUE but no area is provided so the data is not trimmed.")
   }
 
-  return(esri_data)
+  return(data)
 }

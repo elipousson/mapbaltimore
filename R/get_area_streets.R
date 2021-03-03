d36a58b8d6a85307668f5d89bd4e88752b5e456b
Eli Pousson <eli.pousson@gmail.com>
2021-03-02 23:55:48 -0500

    feat: get_streets function added + filter_streets internal function

--- R/get_area_streets.R
+++ R/get_area_streets.R
@@ -37,17 +37,8 @@ get_area_streets <- function(area = NULL,
       diag_ratio = diag_ratio,
       asp = asp,
       trim = trim
-    ) %>%
-      dplyr::mutate(fullname = stringr::str_trim(fullname))
+    )
 
-    # Filter by selected street_type
-    if (!is.null(street_type)) {
-      area_streets <- area_streets %>%
-        dplyr::filter(subtype %in% street_type)
-    } else {
-      area_streets <- area_streets %>%
-        dplyr::filter(subtype != "STRALY")
-    }
   } else {
     # Get streets in area that includes MSA
     area_streets <- get_area_data(
@@ -61,28 +52,77 @@ get_area_streets <- function(area = NULL,
       dplyr::rename(
         fullname = road_name,
         geometry = geom
+      ) %>%
+      dplyr::mutate(
+        subtype = ""
       )
   }
 
+  area_streets <- filter_streets(x = area_streets,
+                                 sha_class = sha_class,
+                                 street_type = street_type,
+                                 union = union)
+
+  return(area_streets)
+}
+
+#' @title Filter streets
+#'
+#' Internal function for filtering streets by multiple parameters
+#'
+#' @param x sf object with streets to filter
+#' @param street_type selected street subtypes to include. By default, the returned data includes all subtypes except alleys ("STRALY"). Options include c("STRALY", "STRPRD", "STRR", "STREX", "STRFIC", "STRNDR", "STRURD", "STCLN", "STRTN"). Not supported for
+#' @param sha_class selected SHA classifications to include. "all" selects all streets with an assigned SHA classification (around one-quarter of all street segments). Additional options include c("COLL", "LOC", "MART", "PART", "FWY", "INT")
+#' @param block_num Integer vector with block number c(300) or range of block numbers (c(100, 500)) to filter streets.
+#' @param union Logical. Default `TRUE`. Union geometry based on `fullname` of streets.
+#' @return streets filtered by parameters
+#' @rdname filter_streets
+#' @importFrom stringr str_to_upper str_trim str_squish
+#' @importFrom dplyr filter mutate group_by summarise
+#' @importFrom sf st_union
+filter_streets <- function(x,
+                           sha_class = NULL,
+                           street_type = NULL,
+                           block_num = NULL,
+                           union = FALSE) {
+
   # Limit to streets with selected SHA classifications
   if (!is.null(sha_class)) {
-    sha_class <- stringr::str_to_upper(sha_class)
+    sha_class_x <- stringr::str_to_upper(sha_class)
 
-    if ("ALL" %in% sha_class) {
-      area_streets <- area_streets %>%
+    if ("ALL" %in% sha_class_x) {
+      x <- x %>%
         dplyr::filter(!is.na(sha_class))
     } else {
-      selected_sha_class <- sha_class
-      area_streets <- area_streets %>%
-        dplyr::filter(sha_class %in% selected_sha_class)
+      x <- x %>%
+        dplyr::filter(sha_class %in% sha_class_x)
     }
   }
 
+  # Filter by selected street_type
+  if (is.null(street_type)) {
+    x <- x %>%
+      dplyr::filter(subtype != "STRALY")
+  } else {
+    x <- x %>%
+      dplyr::filter(subtype %in% street_type)
+  }
+
+  if (!is.null(block_num)) {
+    block_num_x <- block_num
+    x <- x %>%
+      dplyr::filter((block_num >= min(block_num_x)) | block_num == -9,
+                    block_num <= max(block_num_x))
+  }
+
   if (union) {
-    area_streets <- area_streets %>%
+    x <- x %>%
+      dplyr::mutate(
+        fullname = stringr::str_trim(stringr::str_squish(fullname))
+      ) %>%
       dplyr::group_by(fullname) %>%
       dplyr::summarise(geometry = sf::st_union(geometry))
   }
 
-  return(area_streets)
+  return(x)
 }
---
+++ R/get_streets.R
@@ -0,0 +1,69 @@
+#' @title Get streets
+#'
+#' Get streets in Baltimore City by name with option to exclude streets by name,
+#' crop to a bounding box, or to filter to selected street types or functional
+#' classifications.
+#'
+#' @seealso get_area_streets
+#'
+#' @param street_name Street names to return. Required.
+#' @param exclude_name Street names to exclude
+#' @inheritParams filter_streets
+#' @param bbox bbox to crop returned streets. Optional.
+#' @param union Logical. If `TRUE`, use \code{st_union} to combine geometry by
+#'   `fullname` of the streets.
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' get_streets(street_name = "UNIVERSITY PKWY")
+#'
+#' get_streets(street_name = c("E FAYETTE", "ORLEANS"), block_num = c(1700, 3600))
+#' }
+#' @seealso
+#'  \code{\link[mapbaltimore]{streets}}
+#'  \code{\link[mapbaltimore]{get_area_streets}}
+#' @rdname get_streets
+#' @export
+#' @importFrom dplyr filter mutate group_by summarise
+#' @importFrom stringr str_detect str_trim str_squish
+#' @importFrom sf st_union
+
+get_streets <- function(street_name = NULL,
+                        exclude_name = NULL,
+                        street_type = NULL,
+                        sha_class = NULL,
+                        block_num = NULL,
+                        bbox = NULL,
+                        union = TRUE) {
+
+  named_streets <- streets %>%
+    dplyr::filter(
+      stringr::str_detect(
+        fullname,
+        paste0(street_name, collapse = "|")
+      )
+    )
+
+  if (!is.null(exclude_name)) {
+    named_streets <- named_streets %>%
+      dplyr::filter(!stringr::str_detect(
+        fullname,
+        paste0(exclude_name, collapse = "|"))
+      )
+  }
+
+  named_streets <- filter_streets(
+    x = named_streets,
+    sha_class = sha_class,
+    street_type = street_type,
+    block_num = block_num,
+    union = union)
+
+  if (!is.null(bbox)) {
+    named_streets <- named_streets %>%
+      sf::st_bbox(bbox)
+  }
+
+  return(named_streets)
+}

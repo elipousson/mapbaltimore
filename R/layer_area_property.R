f0d994ce7efe88323b2db52e3ce91b0b5e801baf
Eli Pousson <eli.pousson@gmail.com>
2021-04-05 23:04:46 -0400

    feat: Add layer_area_property function

---
+++ R/layer_area_property.R
@@ -0,0 +1,161 @@
+#' Add a layer to a gpplot2 map with area property categorized by type
+#'
+#' Real property or parcel data is from the Maryland State Department of
+#' Assessment and Taxation and may include outdated or inaccurate information.
+#' @param type Real property variable to map. Options include c("improved",
+#'   "vacant", "principal residence", "value"). Currently supports only one
+#'   variable at a time.
+#' @inheritParams layer_area_data
+#' @seealso layer_area_data
+#' @export
+#' @importFrom dplyr case_when mutate filter
+#' @importFrom forcats fct_relevel
+layer_area_property <- function(area = NULL,
+                                bbox = NULL,
+                                data = NULL,
+                                type = c("improved", "vacant", "principal residence", "use", "building type", "value"),
+                                asis = FALSE,
+                                diag_ratio = NULL,
+                                dist = NULL,
+                                asp = NULL,
+                                crop = TRUE,
+                                trim = FALSE,
+                                show_area = FALSE,
+                                show_mask = FALSE,
+                                crs = 2804,
+                                ...) {
+  if (!asis) {
+    cachedata <- "real_property"
+  }
+
+  categorize_area_property <- function(area_property, type) {
+    if (type == "improved") {
+      # Set ordered levels for status variable
+      category_levels <- c(
+        "Improved property",
+        "Unimproved property"
+      )
+
+      area_property <- area_property %>%
+        dplyr::mutate(
+          category = dplyr::case_when(
+            no_imprv == "N" ~ category_levels[[1]],
+            no_imprv == "Y" ~ category_levels[[2]]
+          ),
+          category = forcats::fct_relevel(category, category_levels)
+        )
+    } else if (type == "vacant") {
+
+      # Set ordered levels for status variable
+      category_levels <- c(
+        "Vacant property",
+        "Vacant lot",
+        "Occupied property",
+        "Park/open space"
+      )
+
+      area_property <- area_property %>%
+        dplyr::mutate(
+          category = dplyr::case_when(
+            zonecode == "OS" ~ category_levels[[4]],
+            no_imprv == "Y" ~ category_levels[[2]],
+            vacind == "Y" ~ category_levels[[1]],
+            vacind == "N" ~ category_levels[[3]]
+          ),
+          category = forcats::fct_relevel(category, category_levels)
+        )
+    } else if (type == "principal residence") {
+      category_levels <- c(
+        "Principal residence only",
+        "Principal residence (and another use)",
+        "Not a principal residence"
+      )
+
+      area_property <- area_property %>%
+        dplyr::mutate(
+          category = dplyr::case_when(
+            permhome == "H" ~ category_levels[[1]],
+            permhome == "D" ~ category_levels[[2]],
+            permhome == "N" ~ category_levels[[3]]
+          ),
+          category = forcats::fct_relevel(category, category_levels)
+        )
+    } else if (type == "use") {
+      category_levels <- c(
+        "Residential",
+        "Commercial",
+        "Industrial",
+        "Other"
+      )
+
+      area_property <- area_property %>%
+        dplyr::mutate(
+          category = dplyr::case_when(
+            desclu == "Residential" ~ "Residential",
+            desclu == "Residential Condominium" ~ "Residential",
+            desclu == "Apartments" ~ "Residential",
+            desclu == "Residential/Commercial" ~ "Residential",
+            desclu == "Commercial" ~ "Commercial",
+            desclu == "Commercial Condominium" ~ "Commercial",
+            desclu == "Commercial/Residential" ~ "Commercial",
+            desclu == "Exempt Commercial" ~ "Commercial",
+            desclu == "Industrial" ~ "Industrial",
+            desclu == "Exempt" ~ "Other",
+            TRUE ~ "Other",
+          ),
+          category = forcats::fct_relevel(category, category_levels)
+        )
+    } else if (type == "building type") {
+      stop("building type is not currently supported.")
+      area_property <- area_property %>%
+        mutate(
+          category = stringr::str_to_title(descbldg_cat)
+        )
+    } else if ("category" %in% names(area_property)) {
+      message("Using custom category from data.")
+    } else {
+      stop("The type provided is not currently supported.")
+    }
+
+    return(area_property)
+  }
+
+  if (!asis) {
+    property_layer <-
+      layer_area_data(
+        area = area,
+        cachedata = cachedata,
+        diag_ratio = diag_ratio,
+        dist = dist,
+        asp = asp,
+        crop = crop,
+        trim = trim,
+        fn = ~ .x %>%
+          categorize_area_property(type = type),
+        mapping = aes(fill = category),
+        show_area = show_area,
+        show_mask = show_mask,
+        ...
+      )
+  } else {
+    property_layer <-
+      layer_area_data(
+        area = area,
+        data = data,
+        asis = asis,
+        diag_ratio = diag_ratio,
+        dist = dist,
+        asp = asp,
+        crop = crop,
+        trim = trim,
+        fn = ~ .x %>%
+          categorize_area_property(type = type),
+        mapping = aes(fill = category),
+        show_area = show_area,
+        show_mask = show_mask,
+        ...
+      )
+  }
+
+  return(property_layer)
+}

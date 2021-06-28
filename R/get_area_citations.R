4de4ae19510f39906fb5feb8fa1105cd194679a1
Eli Pousson <eli.pousson@gmail.com>
2021-06-27 20:48:40 -0400

    feat: Improve flexibility of get_area_requests and get_area_citations
    - get_area_requests: Adds support for requests from 2017 to 2020
    - get_area_requests: Allows filtering by agency and request type simultaneously
    - get_area_citations: Filter by description with query instead of after getting data for an area

--- R/get_area_citations.R
+++ R/get_area_citations.R
@@ -1,18 +1,30 @@
 #' @title Get area citations
-#' @description Get Environmental Control Board (ECB) citations.
-#' @param area_type Area type. Requires area_name is also provided. Options include "neighborhood", "council district", or "police district"
-#' @param area_name Area name.Requires area_type is also provided.
-#' @param description String to filter returned citations, e.g. "SIGNS" filters citations to "PROHIBITED POSTING OF SIGNS ON PUBLIC PROPERTY"
-#' @param where string for where condition. Ignore where condition if area_type and area_name are provided.
-#' @inheritParams get_area_esri_data
+#' @description Get Environmental Control Board (ECB) citations from 2007 to
+#'   2021.
+#' @param area_type Area type. Requires area_name is also provided. Options
+#'   include "neighborhood", "council district", or "police district"
+#' @param area_name Area name. Requires area_type is also provided.
+#' @param description String matching description of citations, e.g. "SIGNS"
+#'   filters citations to "PROHIBITED POSTING OF SIGNS ON PUBLIC PROPERTY"
+#' @param where string for where condition. Ignore where condition if area_type
+#'   and area_name are provided.
+#' @param crs Coordinate reference system (CRS) to return. Default 2804
 #' @rdname get_area_citations
+#' @examples
+#' \dontrun{
+#' # Get bulk trash citations for Council District 5
+#' get_area_citations(
+#'   area_type = "council district",
+#'   area_name = "5",
+#'   description = "BULK TRASH")
+#' }
 #' @export
 #' @importFrom snakecase to_any_case
 #' @importFrom stringr str_remove str_trim
 #' @importFrom glue glue
 #' @importFrom esri2sf esri2df
 #' @importFrom janitor clean_names
-#' @importFrom dplyr mutate across filter
+#' @importFrom dplyr select mutate across filter
 #' @importFrom tidyselect ends_with
 #' @importFrom tidyr separate
 #' @importFrom readr parse_number
@@ -22,19 +34,27 @@ get_area_citations <- function(area_type = NULL,
                                description = NULL,
                                where = "1=1",
                                crs = 2804) {
+  if (!is.null(area_type) | !is.null(description)) {
+    area_query <- NULL
+    description_query <- NULL
 
-    # Currently filter by agency *or* request type - not both
-    if (!is.null(area_type)) {
+    if (!is.null(area_type) && !is.null(area_name)) {
       area_type <- match.arg(area_type, c("neighborhood", "council district", "police district"))
       area_type <- snakecase::to_any_case(area_type, case = "big_camel")
 
       if (area_type == "CouncilDistrict") {
-        area_name <- stringr::str_remove(area_name, "District") |>
-        stringr::str_trim()
+        area_name <- stringr::str_remove(area_name, "District[:space:]")
       }
 
-      where <- glue::glue("{area_type} = '{area_name}'")
+      area_query <- glue::glue("{area_type} = '{area_name}'")
     }
+
+    if (!is.null(description)) {
+      description_query <- glue::glue("Description LIKE '%{description}%'")
+    }
+
+    where <- paste0(c(area_query, description_query), collapse = " AND ")
+  }
 
   url <- "https://opendata.baltimorecity.gov/egis/rest/services/NonSpatialTables/ECB/FeatureServer/0"
 
@@ -43,26 +63,25 @@ get_area_citations <- function(area_type = NULL,
     where = where
   ) |>
   janitor::clean_names("snake") |>
+    dplyr::select(-c(esri_oid)) |>
   dplyr::mutate(
-    dplyr::across(tidyselect::ends_with("date"), ~ as.POSIXct(.x / 1000, origin = "1970-01-01"))
+    dplyr::across(where(is.character),
+                  ~ stringr::str_trim(.x)),
+    dplyr::across(tidyselect::ends_with("date"),
+                  ~ as.POSIXct(.x / 1000, origin = "1970-01-01"))
   ) |>
-  tidyr::separate(location, c("lat", "lon"), ",") |>
+  tidyr::separate(location, c("latitude", "longitude"), ",") |>
   dplyr::mutate(
-    lat = readr::parse_number(lat),
-    lon = readr::parse_number(lon)
+    latitude = readr::parse_number(latitude),
+    longitude = readr::parse_number(longitude)
   ) |>
-  dplyr::filter(!is.na(lat)) |>
+  dplyr::filter(!is.na(latitude)) |>
   sf::st_as_sf(
-    coords = c("lon", "lat"),
-    crs = 4326
+    coords = c("longitude", "latitude"),
+    crs = 4326,
+    remove = FALSE
   ) |>
   sf::st_transform(crs)
-
-  if (!is.null(description)) {
-    select_description <- description
-    citations <- citations |>
-    dplyr::filter(str_detect(description, select_description))
-  }
 
   return(citations)
 }
--- R/get_area_requests.R
+++ R/get_area_requests.R
@@ -1,55 +1,117 @@
 #' @title Get area 311 service requests
-#' @description Get 311 service requests for a specific area. Currently only 2021 service requests are supported.
-#' @param year Year for service requests (2021 is currently the only year supported), Default: 2021
+#' @description Get 311 service requests for a specific area. Service requests
+#'   from 2017 to 2020 area available but only a single year can be requested at
+#'   a time. Duplicate requests are removed from the returned data. Requests can
+#'   be filtered by request type, responsible city agency, or both. You can
+#'   return multiple types or agencies, by using a custom where query parameter
+#'   or by calling each type/agency separately.
+#' @param year Year for service requests,
 #' @param request_type Service request type.
-#' @param agency City agency assigned for request. Options include "Transportation", "BGE", "Solid Waste", "Housing", "Water Wastewater", "Health", "Call Center", "Finance", "Liquor Board", "Recreation & Parks", "Fire Department", "Parking Authority", and "General Services"
+#' @param agency City agency responsible for request. Options include
+#'   "Transportation", "BGE", "Solid Waste", "Housing", "Water Wastewater",
+#'   "Health", "Call Center", "Finance", "Liquor Board", "Recreation & Parks",
+#'   "Fire Department", "Parking Authority", and "General Services"
+#' @param where string for where condition. This parameter is ignored if a
+#'   request_type or agency are provided.
 #' @inheritParams get_area_esri_data
+#' @examples
+#' \dontrun{
+#' # Get dirty alley service requests for Edmondson Village in 2020 and 2021
+#' area <- get_area("neighborhood", "Edmondson Village")
+#' requests <-
+#'   map_dfr(
+#'     c(2021, 2020),
+#'     ~ get_area_requests(area = area, year = .x, request_type = "SW-Dirty Alley")
+#'   )
+#' }
 #' @rdname get_area_requests
 #' @export
+#' @importFrom dplyr case_when select rename mutate filter across ends_with
 #' @importFrom glue glue
-#' @importFrom dplyr filter mutate across ends_with case_when select rename
+#' @importFrom esri2sf esri2df
+#' @importFrom janitor clean_names
+#' @importFrom sf st_as_sf st_transform st_intersection
 #' @importFrom stringr str_detect str_remove
 #' @importFrom lubridate int_length interval ymd_hms
 get_area_requests <- function(area,
-                              year = 2021,
+                              year,
                               request_type = NULL,
                               agency = NULL,
+                              where = "1=1",
                               dist = NULL,
                               diag_ratio = NULL,
                               asp = NULL,
                               trim = FALSE,
                               crs = 2804) {
-  if (year == 2021) {
-    path <- "https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/ServiceRequest_311/FeatureServer/0"
-  } else {
-    stop("This function does not currently support any year before to 2021.")
-  }
 
-  where <- NULL
-
-  # Currently filter by agency *or* request type - not both
-  if (!is.null(agency)) {
-    agency <- match.arg(agency, agencies) # Use internal system data for agency list
-    where <- glue::glue("Agency = '{agency}'")
-  } else if (!is.null(request_type)) {
-    request_type <- match.arg(request_type, mapbaltimore::request_types$request_type)
-    where <- glue::glue("SRType = '{request_type}'")
-  }
-
-  if (is.null(where)) {
-    where <- "1=1"
-  }
-
-  requests <- get_area_esri_data(
-    area = area,
-    url = path,
-    where = where,
-    dist = dist,
-    diag_ratio = diag_ratio,
-    asp = asp,
-    trim = trim,
-    crs = crs
+  url <- dplyr::case_when(
+    year == 2021 ~ "https://egis.baltimorecity.gov/egis/rest/services/GeoSpatialized_Tables/ServiceRequest_311/FeatureServer/0",
+    year == 2020 ~ "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2020/FeatureServer/0",
+    year == 2019 ~ "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2019/FeatureServer/0",
+    year == 2018 ~ "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2018/FeatureServer/0",
+    year == 2017 ~ "https://services1.arcgis.com/UWYHeuuJISiGmgXx/arcgis/rest/services/311_Customer_Service_Requests_2017/FeatureServer/0/query?outFields=*&where=1%3D1"
   )
+
+  if (!is.null(agency) | !is.null(request_type)) {
+    agency_query <- NULL
+    request_type_query <- NULL
+
+    if (!is.null(agency)) {
+      agency <- match.arg(agency, agencies) # Use internal system data for agency list
+      agency_query <- glue::glue("Agency = '{agency}'")
+    }
+
+    if (!is.null(request_type)) {
+      request_type <- match.arg(request_type, request_types$request_type)
+      request_type_query <- glue::glue("SRType = '{request_type}'")
+    }
+
+    where <- paste0(c(agency_query, request_type_query), collapse = " AND ")
+  }
+
+  if (year == 2021) {
+    requests <- get_area_esri_data(
+      area = area,
+      url = url,
+      where = where,
+      dist = dist,
+      diag_ratio = diag_ratio,
+      asp = asp,
+      trim = trim,
+      crs = crs
+    ) |>
+    dplyr::select(-c(row_id, needs_sync, is_deleted)) |>
+    dplyr::rename(geometry = geoms)
+  } else if (year %in% c(2020, 2019, 2018, 2017)) {
+    bbox <- area |>
+    adjust_bbox(dist = dist, diag_ratio = diag_ratio, asp = asp, crs = 4326)
+
+    bbox_query <- glue::glue("(latitude >= {bbox$ymin[[1]]}) AND (latitude <= {bbox$ymax[[1]]}) AND (longitude >= {bbox$xmin[[1]]}) AND (longitude <= {bbox$xmax[[1]]})")
+
+    if (where == "1=1") {
+      where <- bbox_query
+    } else {
+      where <- paste0(c(where, bbox_query), collapse = " AND ")
+    }
+
+    requests <- esri2sf::esri2df(
+      url = path,
+      where = where
+    ) |>
+    janitor::clean_names("snake") |>
+    dplyr::mutate(council_district = as.character(council_district)) |>
+    dplyr::filter(!is.na(latitude)) |>
+    sf::st_as_sf(
+      coords = c("longitude", "latitude"),
+      remove = FALSE,
+      crs = 4326
+    ) |>
+    sf::st_transform(crs)
+
+    if (trim) {
+      requests <- sf::st_intersection(requests, area)
+    }
+  }
 
   n_duplicates <- length(dplyr::filter(requests, stringr::str_detect(sr_status, "Duplicate")))
 
@@ -58,19 +120,19 @@ get_area_requests <- function(area,
     requests <- dplyr::filter(requests, !stringr::str_detect(sr_status, "Duplicate")) # Remove duplicates
   }
 
-  requests <- requests %>%
-    # Filter to selected request types
-    dplyr::mutate(
-      # Fix date formatting
-      dplyr::across(dplyr::ends_with("date"), ~ as.POSIXct(.x / 1000, origin = "1970-01-01")),
-      # Calculate the number of days to created to closed
-      days_to_close = dplyr::case_when(
-        sr_status == "Closed" ~ lubridate::int_length(lubridate::interval(lubridate::ymd_hms(created_date), lubridate::ymd_hms(close_date))) / 86400
-      ) %>% round(digits = 2),
-      address = stringr::str_remove(address, ",[:space:](BC$|Baltimore[:space:]City.+)")
-    ) %>%
-    dplyr::select(-c(row_id, sr_record_id, geo_location, needs_sync, is_deleted, police_post)) %>%
-    dplyr::rename(geometry = geoms)
+  requests <- requests |>
+  dplyr::select(-c(sr_record_id, geo_location, police_post)) |>
+  # Filter to selected request types
+  dplyr::mutate(
+    # Fix date formatting
+    dplyr::across(dplyr::ends_with("date"),
+                  ~ as.POSIXct(.x / 1000, origin = "1970-01-01")),
+    # Calculate the number of days to created to closed
+    days_to_close = dplyr::case_when(
+      sr_status == "Closed" ~ lubridate::int_length(lubridate::interval(lubridate::ymd_hms(created_date), lubridate::ymd_hms(close_date))) / 86400
+    ) |> round(digits = 2),
+    address = stringr::str_remove(address, ",[:space:](BC$|Baltimore[:space:]City.+)")
+  )
 
   return(requests)
 }

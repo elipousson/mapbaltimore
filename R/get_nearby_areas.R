e749c1f6948ae81b667fcc191f5b66f744393e93
Eli Pousson <eli.pousson@gmail.com>
2021-08-11 11:16:20 -0400

    feat: Allow get_nearby_areas to return residential neighborhoods only

--- R/get_nearby_areas.R
+++ R/get_nearby_areas.R
@@ -1,18 +1,20 @@
 
 #' Get nearby areas
 #'
-#' Return areas of a selected type within a set distance of
-#' another area.
+#' Return areas of a selected type within a set distance of another area.
 #'
 #' @param area sf object. Must have a name column for exclude_area to work.
 #' @param type Required. Supported values include "neighborhood", "council
 #'   district", "legislative district", "congressional district", "planning
-#'   district", "police district", "csa", and "park district". The type may
-#'   be different than the type of the area provided.
+#'   district", "police district", "csa", and "park district". The type may be
+#'   different than the type of the area provided.
 #' @param dist Distance in meters for matching nearby areas. Default is 1 meter.
 #' @param exclude_area Logical. Default TRUE. If FALSE, include the same areas
 #'   provided to area (assuming the areas provide are the same type as the
 #'   parameter provided to get_nearby_areas).
+#' @param residential Logical. Default FALSE. If the type is neighborhood, set
+#'   TRUE to only return residential neighborhoods (excluding industrial areas,
+#'   business parks, and parks/reservoirs).
 #' @export
 #' @importFrom dplyr filter
 get_nearby_areas <- function(area,
@@ -27,7 +29,8 @@ get_nearby_areas <- function(area,
                                "park district"
                              ),
                              dist = 1,
-                             exclude_area = TRUE) {
+                             exclude_area = TRUE,
+                             residential = FALSE) {
 
   type <- match.arg(type)
 
@@ -35,6 +38,10 @@ get_nearby_areas <- function(area,
 
   if (exclude_area && ("name" %in% names(area))) {
     nearby_areas <- dplyr::filter(nearby_areas, !(name %in% area$name))
+  }
+
+  if (residential && (type == "neighborhood")) {
+    nearby_areas <- dplyr::filter(nearby_areas, type == "Residential")
   }
 
   return(nearby_areas)

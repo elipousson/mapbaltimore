0f520ead3d5243155ac73b32ce9dbdfec781104f
Eli Pousson <eli.pousson@gmail.com>
2021-09-09 08:37:42 -0400

    feat: Add start_date + end_date params to get_area_citations

--- R/get_area_citations.R
+++ R/get_area_citations.R
@@ -6,8 +6,14 @@
 #' @param area_name Area name. Requires area_type is also provided.
 #' @param description String matching description of citations, e.g. "SIGNS"
 #'   filters citations to "PROHIBITED POSTING OF SIGNS ON PUBLIC PROPERTY"
+#' @param start_date Character string in format YYYY-MM-DD. Filters citations by
+#'   violation date.
+#' @param end_date Character string in format YYYY-MM-DD. Filters citations by
+#'   violation date.
 #' @param where string for where condition. Ignore where condition if area_type
 #'   and area_name are provided.
+#' @param geometry Return sf object based on lat/lon. Default TRUE. Set to FALSE
+#'   to return citations with missing coordinates.
 #' @param crs Coordinate reference system (CRS) to return. Default 2804
 #' @rdname get_area_citations
 #' @examples
@@ -31,11 +37,16 @@
 get_area_citations <- function(area_type = NULL,
                                area_name = NULL,
                                description = NULL,
+                               start_date = NULL,
+                               end_date = NULL,
                                where = "1=1",
+                               geometry = TRUE,
                                crs = 2804) {
-  if (!is.null(area_type) | !is.null(description)) {
+  if (!is.null(area_type) | !is.null(description) | !is.null(start_date) | !is.null(end_date)) {
     area_query <- NULL
     description_query <- NULL
+    start_date_query <- NULL
+    end_date_query <- NULL
 
     if (!is.null(area_type) && !is.null(area_name)) {
       area_type <- match.arg(area_type, c("neighborhood", "council district", "police district"))
@@ -52,7 +63,15 @@ get_area_citations <- function(area_type = NULL,
       description_query <- glue::glue("Description LIKE '%{description}%'")
     }
 
-    where <- paste0(c(area_query, description_query), collapse = " AND ")
+    if (!is.null(start_date)) {
+      start_date_query <- glue::glue("ViolationDate >= DATE '{start_date}'")
+    }
+
+    if (!is.null(end_date)) {
+      end_date_query <- glue::glue("ViolationDate <= DATE '{end_date}'")
+    }
+
+    where <- paste0(c(area_query, description_query, start_date_query, end_date_query), collapse = " AND ")
   }
 
   url <- "https://opendata.baltimorecity.gov/egis/rest/services/NonSpatialTables/ECB/FeatureServer/0"
@@ -80,14 +99,18 @@ get_area_citations <- function(area_type = NULL,
   dplyr::mutate(
     latitude = as.numeric(stringr::str_remove(latitude, "\\(|\\)|,")),
     longitude = as.numeric(stringr::str_remove(longitude, "\\(|\\)|,"))
-  ) |>
-  dplyr::filter(!is.na(latitude)) |>
-  sf::st_as_sf(
-    coords = c("longitude", "latitude"),
-    crs = 4326,
-    remove = FALSE
-  ) |>
-  sf::st_transform(crs)
+  )
+
+  if (geometry) {
+    citations <- citations |>
+      dplyr::filter(!is.na(latitude)) |>
+      sf::st_as_sf(
+        coords = c("longitude", "latitude"),
+        crs = 4326,
+        remove = FALSE
+      ) |>
+      sf::st_transform(crs)
+  }
 
   return(citations)
 }

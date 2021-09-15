3703f7a3a875aca4fc5909697ba68adc97c2f14c
Eli Pousson <eli.pousson@gmail.com>
2021-09-14 22:29:45 -0400

    fix: Replace bad MapServer url in get_area_property

--- R/get_area_property.R
+++ R/get_area_property.R
@@ -20,7 +20,7 @@ get_area_property <- function(area = NULL,
                               cache = FALSE,
                               ...) {
 
-  url <- "https://egisdata.baltimorecity.gov/egis/rest/services/Housing/dmxOwnership/MapServer/0"
+  url <- "https://geodata.baltimorecity.gov/egis/rest/services/CityView/Realproperty/MapServer/0"
 
   real_property <-
     get_area_data(
@@ -37,58 +37,24 @@ get_area_property <- function(area = NULL,
       dplyr::across(where(is.character), ~ stringr::str_trim(stringr::str_squish(.x)))
     ) |>
     naniar::replace_with_na_if(is.character, ~ .x == "") |>
-    dplyr::select(
-      block,
-      lot,
-      blocklot,
+    dplyr::rename(
       full_address = fulladdr,
       bldg_num = bldg_no,
-      fraction,
-      span_num,
       street_dirpre = stdirpre,
       street_name = st_name,
       street_type = st_type,
-      zip_code,
       zip_code_ext = extd_zip,
-      permhome,
-      no_imprv,
       dhcd_use = dhcduse1,
       resp_agency = respagcy,
       neighborhood = neighbor,
       sale_price = salepric,
       sale_date = saledate,
-      owner_1,
-      owner_2,
-      owner_3,
-      taxbase,
-      fullcash,
-      permhome,
-      no_imprv,
       zoning = zonecode,
       year_built = year_build,
-      sdatlink,
       geometry = geoms
     ) |>
     naniar::replace_with_na(replace = list(year_built = 0)) |>
-    tidyr::replace_na(replace = list(no_imprv = "N"))
-
-  vacants <-
-    get_area_vacants(
-      area = area,
-      bbox = bbox,
-      dist = dist,
-      diag_ratio = diag_ratio,
-      asp = asp,
-      crop = crop,
-      trim = trim
-    )
-
-  real_property <-
-    real_property |>
-    dplyr::mutate(
-      vacind = if_else(blocklot %in% vacants$blocklot, "Y", "N"),
-      .after = no_imprv
-    )
+    tidyr::replace_na(replace = list(no_imprv = "N", vacind = "N"))
 
   if (cache) {
     cache_baltimore_data(data = real_property, ...)

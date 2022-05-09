28ee679fbb3e32e1a922c5b9fdcdaf1c5b2db3cc
Eli Pousson <eli.pousson@gmail.com>
2022-05-08 21:12:07 -0400

    refactor: add erase_parks and erase_water functions

--- R/utils.R
+++ R/utils.R
@@ -2,8 +2,8 @@
 #'
 #' @param package Name of a package.
 #' @param repo GitHub repository to use for the package.
-#' @importFrom rlang is_installed check_installed
 #' @noRd
+#' @importFrom rlang is_installed check_installed
 is_pkg_installed <- function(pkg, repo = NULL) {
   if (!rlang::is_installed(pkg = pkg)) {
     if (!is.null(repo)) {
@@ -12,4 +12,30 @@ is_pkg_installed <- function(pkg, repo = NULL) {
 
     rlang::check_installed(pkg = pkg)
   }
+}
+
+
+#' @noRd
+#' @importFrom overedge st_erase get_location
+erase_parks <- function(x) {
+  overedge::st_erase(
+    x,
+    overedge::get_location(
+      type = neighborhoods,
+      name_col = "type",
+      name = "Park/open space",
+      union = TRUE
+    )
+  )
+}
+
+
+
+#' @noRd
+#' @importFrom overedge st_erase get_location
+erase_water <- function(x) {
+  overedge::st_erase(
+    x,
+    sf::st_union(baltimore_msa_water)
+  )
 }

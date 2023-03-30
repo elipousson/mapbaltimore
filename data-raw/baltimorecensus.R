3decd05205b29c11a6825646c2c85d6ad2317714
Eli Pousson <eli.pousson@gmail.com>
2023-03-29 23:17:42 -0400

    refactor: use baltimorecensus to re-export data not functions

---
+++ data-raw/baltimorecensus.R
@@ -0,0 +1,19 @@
+baltimore_blocks <- baltimorecensus::baltimore_blocks
+
+usethis::use_data(baltimore_blocks, overwrite = TRUE)
+
+baltimore_block_groups <- baltimorecensus::baltimore_block_groups
+
+usethis::use_data(baltimore_block_groups, overwrite = TRUE)
+
+baltimore_tracts <- baltimorecensus::baltimore_tracts
+
+usethis::use_data(baltimore_tracts, overwrite = TRUE)
+
+baltimore_pumas <- baltimorecensus::baltimore_pumas
+
+usethis::use_data(baltimore_pumas, overwrite = TRUE)
+
+baltimore_msa_counties <- baltimorecensus::baltimore_msa_counties
+
+usethis::use_data(baltimore_msa_counties, overwrite = TRUE)

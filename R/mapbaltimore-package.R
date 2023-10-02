33b4b3c0dd50299c8e10a9076ea939190f3cdcac
your name <eli.pousson@gmail.com>
2023-10-02 13:16:07 -0400

    refactor: import standalone type check functions

--- R/mapbaltimore-package.R
+++ R/mapbaltimore-package.R
@@ -4,9 +4,9 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @import rlang
 #' @importFrom cli cli_abort cli_warn cli_inform
 #' @importFrom glue glue glue_collapse
 #' @importFrom lifecycle deprecated
-#' @importFrom rlang .data is_null check_installed check_required
 ## usethis namespace: end
 NULL

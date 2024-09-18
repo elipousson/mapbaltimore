661c7082030e8c0a16890dc268c4e720b5193663
Eli Pousson <eli.pousson@gmail.com>
2024-09-18 11:39:30 -0400

    Update request examples
    Not working at present

--- examples/get_area_requests.R
+++ examples/get_area_requests.R
@@ -1,12 +1,14 @@
 # Get boundary for Edmondson Village
-area <- get_area("neighborhood", "Edmondson Village")
+area <- get_baltimore_area("neighborhood", "Edmondson Village")
 
 # Get fallen limb requests for 2022
-get_area_requests(
+fallen_limbs <- get_area_requests(
   area = area,
-  date_range = c("2022-11-01", "2022-12-31"),
+  date_range = c("2022-11-01", "2022-11-15"),
   request_type = "FOR-Fallen Limb"
 )
+
+dplyr::glimpse(fallen_limbs)
 
 # Get dirty alley service requests for multiple years using purrr::map_dfr()
 purrr::list_rbind(
